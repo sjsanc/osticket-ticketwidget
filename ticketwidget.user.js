29426d4d5cbea92a9b7392bab2e0121e089b95a8
sjsanc <52794806+sjsanc@users.noreply.github.com>
2020-05-27 22:03:54 +0100

    Create ticketwidget.user.js

---
+++ ticketwidget.user.js
@@ -0,0 +1,176 @@
+// ==UserScript==
+// @name        osticket-ticketwidget 
+// @namespace   Violentmonkey Scripts
+// @match       http://gbuk-ticketsystem/osticket/scp/*
+// @grant       none
+// @version     1.0
+// @author      sjs
+// @require     https://kit.fontawesome.com/2c7fa631b5.js
+// @description A little dashboard widget for all your queues to relax in. Neat!
+// ==/UserScript==
+
+let queues = [] // array of queue objects
+
+const setStorage = () => {
+  localStorage.data = JSON.stringify(queues);
+}
+
+const getStorage = () => {
+  queues = JSON.parse(localStorage.data);
+}
+
+getStorage();
+
+const Queue = function(name, queueId) {
+  this.name=name; 
+  this.queueId="q"+queueId;
+  this.ticketCount= "-";
+  this.id=queues.length;
+  queues.push(this);
+}
+
+// test queues
+// const queue1 = new Queue("Unassinged Tickets", 34);
+// const queue2 = new Queue("Total Tickets", 6);
+
+async function userData() {
+  let response = await fetch('http://gbuk-ticketsystem/osticket/scp/ajax.php/queue/counts');
+  let data = await response.json();
+  return data;
+}
+
+const getData = function() {
+  // update queue objects with latest ticket amount;
+  userData().then(data => {
+  queues.forEach(queue => {
+    queue.ticketCount = data[queue.queueId]
+    updateTickets();
+  })
+  console.log(queues);
+ });
+};
+
+const deleteQueue = function() {
+  const q = event.target.parentNode.parentNode //
+  const qId = q.id;
+  
+  q.parentNode.removeChild(q)
+  queues.splice(q.id,1);
+  setStorage();
+}
+
+const createQueue = function(){
+  const queueId = Number(document.getElementById('queueId').value);
+  const name = document.getElementById('queueName').value;
+  new Queue(name, queueId);
+  console.log("Queue created! id: "+ queueId + " Name: " + name);
+  renderQueues()
+  setStorage();
+}
+
+const editQueue = function() {
+  const editInput = prompt("New queue name: ");
+  if(editInput == "") alert("Can't be empty. D'oh!")
+  
+  const text = event.target.parentNode.childNodes[2];
+  text.innerText = editInput;
+  
+  setStorage();
+}
+
+const renderWidget = function(){
+  const widgetDiv = document.createElement('div')
+  const widgetDivContent = `
+    
+    <div class="w-container" id="wContainer">
+      <div class="w-header">
+        <h1>Ticket Widget</h1>
+        <p>2</p>
+        <i class="fas fa-info" id="infoIcon"></i>
+      </div>
+
+      <div class="w-new-queue">
+        <input type="number" placeholder="id" id="queueId" max-length="4">
+        <input type="text" placeholder="queue" id="queueName">
+        <button id="submitQueue">Create</button>
+      </div>
+
+      <div class="w-queue-body" id="wQueueBody">
+
+      </div>
+    </div>
+  `
+  widgetDiv.innerHTML = widgetDivContent;
+  document.body.appendChild(widgetDiv);
+  
+  // add listeners because userscripts suck
+  document.getElementById('submitQueue').addEventListener('click', createQueue);
+}; renderWidget();
+
+const renderQueues = function(){
+  const qBody = document.getElementById('wQueueBody');
+  
+  // empty out queuelist to prevent concatenation
+  while(qBody.firstChild) qBody.removeChild(qBody.firstChild);
+
+  // generate element for each queue
+  queues.forEach(queue => {
+    const qDiv = document.createElement('div')
+    qDiv.id = queue.id
+    qDiv.classList = "w-queue";
+    const q = `
+        <div><i class="fas fa-times w-delete"></i><i class="fas fa-edit w-edit"></i><a class="hackiestClassEver" href="http://gbuk-ticketsystem/osticket/scp/tickets.php?queue=${queue.id}">${queue.name}</a></div>
+        <div id=${queue.queueId} class="w-amount">0</div>
+        `
+    qDiv.innerHTML = q;
+    qBody.appendChild(qDiv);
+    
+    // add event listeners, because userscripts suck
+    const deletes = document.getElementsByClassName('w-delete');
+    for(let i = 0; i < deletes.length; i++) deletes[i].addEventListener('click', deleteQueue);
+    const edits = document.getElementsByClassName('w-edit');
+    for(let i = 0; i < edits.length; i++) edits[i].addEventListener('click', editQueue);
+ }) 
+};
+renderQueues();
+
+const renderInfoBox = function() {
+  const infoDiv = document.createElement('div');
+  infoDiv.id = "infoDiv"
+  infoDiv.classList = "w-info-div"
+
+  const info = `
+  <p>To add a queue, find the queue id by hovering over its entry in the dropdown menues above, then look at the address that shows up at the bottom of your browser</p>
+  `
+  infoDiv.innerHTML = info
+  document.getElementById('wContainer').appendChild(infoDiv);
+};renderInfoBox(); 
+
+const showInfo = function() {
+  infoDiv = document.getElementById('infoDiv')
+  infoDiv.classList.toggle('shown');
+  console.log("something is happenign!!")
+}
+
+document.getElementById('infoIcon').addEventListener('mouseenter', showInfo);
+document.getElementById('infoIcon').addEventListener('mouseout', showInfo);
+
+const updateTickets = function() {
+  // update DOM with ticket values
+  queues.forEach(queue => {
+    const q = document.getElementById(queue.queueId);
+    q.innerText = queue.ticketCount;
+    console.log("Updated DOM");
+  })
+}; 
+
+
+
+
+
+getData(); // get data on page load
+
+setInterval(getData, 30000);// refresh ticket values every 30s
+
+
+
